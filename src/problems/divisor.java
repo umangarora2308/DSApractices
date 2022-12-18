2e4f1de1717a2bd7291f322ee77af7963fa6ee0b
Umang <umangarora08@gmail.com>
2022-12-18 15:21:28 +0530

    divisor.java

---
+++ src/problems/divisor.java
@@ -0,0 +1,20 @@
+package problems;
+
+import java.util.Scanner;
+
+public class divisor {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        divisorfind(n);
+    }
+
+    public static void divisorfind(int n) {
+        for (int i = 1; i <=n ; i++) {
+            if (n%i==0) {
+                System.out.println(i);
+            }
+
+        }
+    }
+}
